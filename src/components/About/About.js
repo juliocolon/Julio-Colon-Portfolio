aa158af0fdd02cb0ef7082c6bb7374d95b8376da
Julio Colon <jcolon1092@gmail.com>
2022-06-10 19:25:00 -0400

    adjusting css

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,7 +8,7 @@ export default function About() {
                 <img className={styles.image} src="/Julio_Headshot.jpeg" alt="Julio-Headhsot" />
                 <h1 className={styles.heading}>Hi, I'm Julio</h1>
             </span>
-            <span className={styles.statement}>I'm a Marketing Professional & Full-Stack Software Engineer specializing in the MERN Stack.</span>
+            <span className={styles.statement}>I'm a Marketing Professional & Software Engineer specializing in the MERN Stack.</span>
             <div className={styles.text}>
                 <p>My goal is to build intuitive, user-first, digital experiences.</p>
                 <p>To learn more about my professional background, click here to view my <a className={styles.anchor} href="https://pdfhost.io/v/gugwBMwm._Julio_Colon_Resume" target="_blank">resume</a>.</p>
--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -9,28 +9,28 @@ export default function ProjectCard() {
    <>    
    <h1 className={styles.heading}>Current Projects</h1>
 <div className={styles.cards}>
- <Card style={{ width: '20rem', height:'34rem', margin: "3%" }}>
+ <Card style={{ width: '20rem', height:'36rem', margin: "3%" }}>
         <Card.Img className={styles.image} variant="top" src="/Boston Celtics Trivia.png" />
         <Card.Body>
           <Card.Title className={styles.title}>Boston Celtics Trivia</Card.Title>
           <Card.Text className={styles.cardtext}>
            Browser based trivia game based on the history of the Boston Celtics. The game features a win and lose state. Built using <em>HTML</em>, <em>CSS</em>, <em>Vanilla JavaScript</em> and <em>GitHub</em> for version control. 
           </Card.Text>
-          <Button href="https://github.com/juliocolon/Boston-Celtics-Trivia-" target="_blank" variant="secondary">View Project</Button>
+          <Button  className={styles.button} href="https://github.com/juliocolon/Boston-Celtics-Trivia-" target="_blank" variant="secondary">View Project</Button>
         </Card.Body>
       </Card>
-      <Card style={{ width: '20rem', height:'34rem', margin: "3%"  }}>
+      <Card style={{ width: '20rem', height:'36rem', margin: "3%"  }}>
       <Card.Img className={styles.image} variant="top" src="/sneaker forum.png" />
       <Card.Body>
         <Card.Title className={styles.title}>Sneaker Forum</Card.Title>
         <Card.Text className={styles.cardtext}>
-          Sneaker Forum, allows users to to perform full CRUD operations on threads they create on different topics concerning sneakers. 
+          Sneaker Forum, allows users to perform full CRUD operations on threads they create on different topics concerning sneakers. 
           This application was built using MongoDB, Express, React.js, React Bootstrap, and Node.js. This application also features JWT authentication. 
         </Card.Text>
-        <Button variant="secondary">View Project</Button>
+        <Button   className={styles.button} href="https://github.com/juliocolon/SEI-Project-3-Sneaker-Forum" target="_blank" variant="secondary">View Project</Button>
       </Card.Body>
     </Card>
-    <Card style={{ width: '20rem', height:'34rem', margin: "3%" }}>
+    <Card style={{ width: '20rem', height:'36rem', margin: "3%" }}>
     <Card.Img className={styles.image} variant="top" src="/The Roaming Spoon.png" />
     <Card.Body id="card">
       <Card.Title className={styles.title}>The Roaming Spoon</Card.Title>
@@ -38,7 +38,7 @@ export default function ProjectCard() {
         The Roaming Spoon is a Yelp like application for food trucks. This application was built using MongoDB, Express, React.js, React Bootstrap and Node.js. This application also features JWT authentication. 
         The Roaming Spoon was created in collaboration with General Assembly Software Engineering and UI/UX students.
       </Card.Text>
-      <Button href="https://github.com/juliocolon/roaming-food-trucks/tree/main" target="_blank" variant="secondary">View Project</Button>
+      <Button className={styles.button} href="https://github.com/juliocolon/roaming-food-trucks/tree/main" target="_blank" variant="secondary">View Project</Button>
     </Card.Body>
   </Card>
 </div>
