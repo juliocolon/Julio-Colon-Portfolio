f6c3f2be78a28a219b928acb1b189d166e8ce9e7
Julio Colon <jcolon1092@gmail.com>
2022-06-11 10:49:13 -0400

    changes

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -22,7 +22,7 @@ export default function ProjectCard() {
       <Card style={{ width: '20rem', height:'36rem', margin: "3%"  }}>
       <Card.Img className={styles.image} variant="top" src="/sneaker forum.png" />
       <Card.Body>
-        <Card.Title className={styles.title}> <a className={styles.link} href="https://roaming-spoon.herokuapp.com/" target="_blank">Sneaker Forum</a></Card.Title>
+        <Card.Title className={styles.title}> <a className={styles.link} href="https://sneakers-forum.herokuapp.com/" target="_blank">Sneaker Forum</a></Card.Title>
         <Card.Text className={styles.cardtext}>
           Sneaker Forum, allows users to perform full CRUD operations on threads they create on different topics concerning sneakers. 
           This application was built using MongoDB, Express, React.js, React Bootstrap, and Node.js. This application also features JWT authentication. 
@@ -33,7 +33,7 @@ export default function ProjectCard() {
     <Card style={{ width: '20rem', height:'36rem', margin: "3%" }}>
     <Card.Img className={styles.image} variant="top" src="/The Roaming Spoon.png" />
     <Card.Body id="card">
-      <Card.Title className={styles.title}><a className={styles.link}  href="" target="_blank">The Roaming Spoon</a></Card.Title>
+      <Card.Title className={styles.title}><a className={styles.link}  href="https://roaming-spoon.herokuapp.com/" target="_blank">The Roaming Spoon</a></Card.Title>
       <Card.Text className={styles.cardtext}>
         The Roaming Spoon is a Yelp like application for food trucks. This application was built using MongoDB, Express, React.js, React Bootstrap and Node.js. This application also features JWT authentication. 
         The Roaming Spoon was created in collaboration with General Assembly Software Engineering and UI/UX students.
